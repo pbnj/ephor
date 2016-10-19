cd3721651e5bcbd69efb4f2829bbc43e8c161dbd
Garrett Graupmann <garrett_graupmann@intuit.com>
2016-10-18 17:33:40 -0700

    Added more console commands.

--- main.go
+++ main.go
@@ -75,14 +75,20 @@ func checkError(e error) {
 	return
 }
 
-/* Prints the help command for use in the console
+/* Prints the 'help' output for use in the console; this should print any time
+ * the user inputs an incorrect command
  * @params - none
  * @return - none
  */
 func consoleHelp() {
 	fmt.Println("Ephor Console Commands:")
-	fmt.Println("  exit			Exits the console.")
-	fmt.Println("  help			Prints this help message.")
+	fmt.Println(" config filename     Reloads the console with the specified config file.")
+	fmt.Println(" exit                Exits the Ephor console.")
+	fmt.Println(" file filename       Writes the output to the specified file.")
+	fmt.Println(" help                Prints this help message.")
+	fmt.Println(" output filetype     Changes the output file type to the specified type (XML/JSON/CSV).")
+	fmt.Println(" query querystring   Runs the specified query and outputs the results.")
+	return
 }
 
 /* Runs the console for interactive mode. This function loops, executing each
@@ -93,10 +99,10 @@ func consoleHelp() {
 func consoleRun() {
 	// create the client connection
 	fmt.Printf("Connecting to splunk instance '%s'... ", urlAddr)
-	tr := &http.Transport{
-		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-	}
-	client := &http.Client{Transport: tr}
+	// tr := &http.Transport{
+	// 	TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	// }
+	// client := &http.Client{Transport: tr}
 	fmt.Println("connected.")
 	for {
 		fmt.Print("ephor > ")
@@ -104,14 +110,43 @@ func consoleRun() {
 		in, _ := r.ReadString('\n')
 		consoleCmd := strings.Split(strings.TrimSuffix(in, "\n"), " ")
 		switch consoleCmd[0] {
+		case "config":
+
 		case "exit":
 			return
-		case "test":
-			fmt.Println(client)
+		case "file":
+			if len(consoleCmd) != 2 {
+				consoleUsage()
+			} else {
+				file = consoleCmd[1]
+				fmt.Println("Data will now be output to " + file)
+			}
+		case "output":
+			if len(consoleCmd) != 2 || !validOutputType(consoleCmd[1]) {
+				consoleUsage()
+			} else {
+				output = consoleCmd[1]
+				fmt.Println("Data will now be output as " + strings.ToUpper(output))
+			}
+		case "query":
+			if file == "" {
+				fmt.Fprint(os.Stdout, "Somthing was output")
+			} else {
+				ioutil.WriteFile(file, []byte("Something was output"), 0644)
+			}
 		default:
 			consoleHelp()
 		}
 	}
+}
+
+/* Print a usage message and then print the consoleHelp message
+ * @params - none
+ * @return - none
+ */
+func consoleUsage() {
+	fmt.Println("Incorrect command usage - see help below")
+	consoleHelp()
 }
 
 /* This init function handles the parsing of the commandline flags and the
@@ -173,8 +208,18 @@ func init() {
 	if output == "" {
 		output = "xml" // splunk docs note the default datatype is XML
 	}
-	if !strings.EqualFold("XML", output) && !strings.EqualFold("JSON", output) && !strings.EqualFold("CSV", output) {
+	if !validOutputType(output) {
 		fmt.Fprintf(os.Stderr, "ERROR: output format ('%s') is invalid; valid values are XML (default), JSON, and CSV\n", output)
 		output = "xml" // splunk docs note the default datatype is XML
 	}
+}
+
+/* Returns true if the provided string is equal to "XML", "JSON", or "CSV",
+ * ignoring character case
+ * @params - t: a string value to validate
+ * @return - a boolean indicating whether the provided sting is a valid
+ * 			 output type
+ */
+func validOutputType(t string) bool {
+	return strings.EqualFold("XML", t) || strings.EqualFold("JSON", t) || strings.EqualFold("CSV", t)
 }
